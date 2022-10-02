705e2cf2f6b3004dfd1106d51c41bb37aa34a96b
Harrison <koharrisonko@gmail.com>
2022-10-02 01:52:57 -0500

    Added functional pause menu

--- scifi/Assets/Scripts/PauseManager.cs
+++ scifi/Assets/Scripts/PauseManager.cs
@@ -2,10 +2,20 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
+using UnityEngine.SceneManagement;
 
 public class PauseManager : MonoBehaviour
 {
     public bool paused = false;
+    [SerializeField]
+    private Canvas canvas;
+    [SerializeField]
+    private Canvas pauseCanvas;
+    public void Start()
+    {
+        pauseCanvas.GetComponent<Canvas>().enabled = false;
+    }
+
     public void OnClick()
     {
         if(!paused)
@@ -16,13 +26,29 @@ public class PauseManager : MonoBehaviour
 
     public void PauseGame()
     {
+        canvas.GetComponent<Canvas>().enabled = false;
+        pauseCanvas.GetComponent<Canvas>().enabled = true;
         Time.timeScale = 0;
         paused = true;
     }
 
     public void ResumeGame()
     {
+        canvas.GetComponent<Canvas>().enabled = true;
+        pauseCanvas.GetComponent<Canvas>().enabled = false;
         Time.timeScale = 1;
         paused = false;
+    }
+
+    public void MainMenu()
+    {
+        Time.timeScale = 1;
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex - 1);
+    }
+
+    public void Retry()
+    {
+        Time.timeScale = 1;
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
     }
 }
